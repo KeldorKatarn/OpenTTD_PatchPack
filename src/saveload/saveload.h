e62814ad742999b71ef7921286e59643dc8514f4
keldorkatarn <schmitta79@gmail.com>
2016-01-11 10:47:50 +0100

    Removed the 'timetable_in_ticks' settings as it only makes things less accurate for the player. The values were even rounded up to accomodate this. It will also clash with the daylength in terms of which value should be displayed. The more accurate default daylength or the less confusing but horribly inaccurate extended daylength.

--- src/saveload/saveload.h
+++ src/saveload/saveload.h
@@ -104,6 +104,7 @@ enum SLRefType {
 #define SL_PATCH_PACK_1_8  266
 #define SL_PATCH_PACK_1_9  267
 #define SL_PATCH_PACK_1_10 268
+#define SL_PATCH_PACK_1_11 269
 
 /** Flags of a chunk. */
 enum ChunkType {

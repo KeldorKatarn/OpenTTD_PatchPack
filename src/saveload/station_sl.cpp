7719c650ea31a7397f57d4eca97bd1f945674a40
keldorkatarn <schmitta79@gmail.com>
2016-01-12 20:13:28 +0100

    - Added a detailed station rating tool tip for each cargo in the station window.
    - Also changed the way punishments for source stations work. That is now also being displayed in the tool tip.

--- src/saveload/station_sl.cpp
+++ src/saveload/station_sl.cpp
@@ -287,6 +287,7 @@ const SaveLoad *GetGoodsDesc()
 		 SLE_CONDVAR(GoodsEntry, node,                 SLE_UINT16,                183, SL_MAX_VERSION),
 		SLEG_CONDVAR(            _num_flows,           SLE_UINT32,                183, SL_MAX_VERSION),
 		 SLE_CONDVAR(GoodsEntry, max_waiting_cargo,    SLE_UINT32,                183, SL_MAX_VERSION),
+		 SLE_CONDVAR(GoodsEntry, was_punished,         SLE_BOOL,   SL_PATCH_PACK_1_12, SL_MAX_VERSION),
 		SLE_END()
 	};
 

56b6cff75294b177f8ad551c427f6b9fb9260bff
keldorkatarn <schmitta79@gmail.com>
2016-01-14 13:21:17 +0100

    Fixed the crash when logic signals are autobuilt by dragging.

--- src/rail_cmd.cpp
+++ src/rail_cmd.cpp
@@ -1239,6 +1239,11 @@ CommandCost CmdBuildSingleSignal(TileIndex tile, DoCommandFlag flags, uint32 p1,
 			SetPresentSignals(tile, (GetPresentSignals(tile) & ~SignalOnTrack(track)) | (p2 & SignalOnTrack(track)));
 			SetSignalVariant(tile, track, sigvar);
 			SetSignalType(tile, track, sigtype);
+
+			/* logic signals: create a new signal program */
+			if (sigtype == SIGTYPE_LOGIC) {
+				CreateSignalProgram(tile, track);
+			}
 		}
 
 		/* Add new signal infrastructure count. */

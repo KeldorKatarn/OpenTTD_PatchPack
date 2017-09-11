44813c8429ba684b03c5e77e1650ec2d88e52e33
keldorkatarn <schmitta79@gmail.com>
2017-09-11 08:43:24 +0200

    Add 'Minimum distance between towns' world generation setting.

--- src/saveload/saveload.cpp
+++ src/saveload/saveload.cpp
@@ -287,8 +287,9 @@
  *  271   SL_PATCH_PACK_1_14
  *  272   SL_PATCH_PACK_1_15
  *  273   SL_PATCH_PACK_1_16
+ *  274   SL_PATCH_PACK_1_18
  */
-extern const uint16 SAVEGAME_VERSION = SL_PATCH_PACK_1_16; ///< Current savegame version of OpenTTD.
+extern const uint16 SAVEGAME_VERSION = SL_PATCH_PACK_1_18; ///< Current savegame version of OpenTTD.
 
 SavegameType _savegame_type; ///< type of savegame we are loading
 FileToSaveLoad _file_to_saveload; ///< File to save or load in the openttd loop.

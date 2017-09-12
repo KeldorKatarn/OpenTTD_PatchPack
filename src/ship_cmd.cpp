acbff5528b6aa5a01d17b814a44b6d884ee03f69
keldorkatarn <schmitta79@gmail.com>
2017-09-12 04:53:59 +0200

    Simplify direction lookup to also work for oil rigs.

--- src/ship_cmd.cpp
+++ src/ship_cmd.cpp
@@ -323,9 +323,7 @@ TileIndex Ship::GetOrderStationLocation(StationID station)
 		} else {
 			const Dock* dock = GetBestDock(this, st);
 
-			DiagDirection direction = GetInclinedSlopeDirection(GetTileSlope(dock->sloped));
-			direction = ReverseDiagDir(direction);
-
+			DiagDirection direction = DiagdirBetweenTiles(dock->sloped, dock->flat);
 			return dock->flat + TileOffsByDiagDir(direction);
 		}
 	} else {

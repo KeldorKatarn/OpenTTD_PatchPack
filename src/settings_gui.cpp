92de23cf6f0e9713532d26ce0a296a5df5c204dc
keldorkatarn <schmitta79@gmail.com>
2017-09-17 16:02:39 +0200

    Make min_river_length setting visible in the GUI.

--- src/settings_gui.cpp
+++ src/settings_gui.cpp
@@ -1691,6 +1691,7 @@ static SettingsContainer &GetSettingsTree()
 			genworld->Add(new SettingEntry("game_creation.variety"));
 			genworld->Add(new SettingEntry("game_creation.snow_line_height"));
 			genworld->Add(new SettingEntry("game_creation.amount_of_rivers"));
+			genworld->Add(new SettingEntry("game_creation.min_river_length"));
 			genworld->Add(new SettingEntry("game_creation.tree_placer"));
 			genworld->Add(new SettingEntry("vehicle.road_side"));
 			genworld->Add(new SettingEntry("economy.larger_towns"));

b19504c393637140b75bfdf66068f4f216b81a4e
keldorkatarn <schmitta79@gmail.com>
2018-04-07 18:07:17 +0200

    When building tunnels, open new viewports at the far end of the tunnel.

--- src/viewport_gui.cpp
+++ src/viewport_gui.cpp
@@ -14,6 +14,8 @@
 #include "window_gui.h"
 #include "viewport_func.h"
 #include "strings_func.h"
+#include "tunnelbridge.h"
+#include "tilehighlight_func.h"
 #include "zoom_func.h"
 #include "window_func.h"
 #include "gfx_func.h"
@@ -187,11 +189,17 @@ void ShowExtraViewPortWindow(TileIndex tile)
 
 /**
  * Show a new Extra Viewport window.
- * Center it on the tile under the cursor, if the cursor is inside a viewport.
+ * When building a tunnel, the tunnel end-tile is used as center for new viewport.
+ * Otherwise center it on the tile under the cursor, if the cursor is inside a viewport.
  * If that fails, center it on main viewport center.
  */
 void ShowExtraViewPortWindowForTileUnderCursor()
 {
+	if (_build_tunnel_endtile != 0 && _thd.place_mode & HT_TUNNEL) {
+		ShowExtraViewPortWindow(_build_tunnel_endtile);
+		return;
+	}
+
 	/* Use tile under mouse as center for new viewport.
 	 * Do this before creating the window, it might appear just below the mouse. */
 	Point pt = GetTileBelowCursor();

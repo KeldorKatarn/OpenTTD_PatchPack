14722e0865c5f289f519c1f18394a331a65e5969
keldorkatarn <schmitta79@gmail.com>
2018-04-08 14:34:49 +0200

    TBTR: Remove unused const variable.

--- src/tbtr_template_vehicle_func.cpp
+++ src/tbtr_template_vehicle_func.cpp
@@ -33,7 +33,6 @@
 #include <stdio.h>
 
 Vehicle *vhead, *vtmp;
-static const uint MAX_ARTICULATED_PARTS = 100;
 
 #ifdef _DEBUG
 // debugging printing functions for convenience, usually called from gdb

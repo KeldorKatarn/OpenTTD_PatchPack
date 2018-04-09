f436b56d6a517a9996002ecec36414f02bce9deb
keldorkatarn <schmitta79@gmail.com>
2018-04-09 17:34:04 +0200

    Viewport order lines refresh fix for re-displaying previous orders.

--- src/viewport.cpp
+++ src/viewport.cpp
@@ -1812,6 +1812,7 @@ static void ViewportMapDrawVehicleRoute(const ViewPort *vp)
 			// make sure we remove any leftover paths
 			MarkRoutePathsDirty(_vp_route_paths);
 			_vp_route_paths.clear();
+			_vp_route_paths_last_mark_dirty.clear();
 			DEBUG(misc, 1, "ViewportMapDrawVehicleRoute: redrawing dirty paths 0");
 		}
 		return;
